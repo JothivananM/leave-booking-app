ba856ffe5ee8b9f5ff062f68a387fc3b8aabf0e3
JothivaananM <jothivaanan.madheswaran@nipurnait.com>
2023-03-09 15:18:34 +0530

    new

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -28,6 +28,8 @@ import { deleteUser } from '../service/axios';
 import SlidingPane from "react-sliding-pane";
 import "react-sliding-pane/dist/react-sliding-pane.css";
 import { useConfirm } from "material-ui-confirm";
+import Typography from '@mui/material/Typography';
+
 
 const Alert = React.forwardRef(function Alert(props, ref) {
     return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
@@ -182,7 +184,20 @@ export default function Home() {
 
     return (
         <>
-            <Box height={100} />
+            <Box height={80} />
+            <Typography paragraph>
+            Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
+            eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
+            neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
+            tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
+            sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
+            tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
+            gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
+            et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
+            tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+            eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+            posuere sollicitudin aliquam ultrices sagittis orci a.
+          </Typography>
             <div>
                 <DataGrid
                     autoHeight={true}
